7b14a6ce5ca52f077eecd0ec89618e72e57d26a9
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-05-20 12:02:29 -0500

    Inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taller_Conexion_Gemini_Ai_Gpt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
