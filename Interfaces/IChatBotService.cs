8e21035b41f02b463db2ed1e17d60132b4e5f94c
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-05-20 13:07:42 -0500

    Se agregaron las interfaces

---
+++ Interfaces/IChatBotService.cs
@@ -0,0 +1,7 @@
+﻿namespace Taller_Conexion_Gemini_Ai_Gpt.Interfaces
+{
+    public interface IChatBotService
+    {
+        Task<string> GetChatBotResponse(string prompt, string provider);
+    }
+}
---
+++ Interfaces/IGroqRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Taller_Conexion_Gemini_Ai_Gpt.Interfaces
+{
+    public interface IGroqRepository
+    {
+        Task<string> GetResponseAsync(string prompt);
+    }
+}
